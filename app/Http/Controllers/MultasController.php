bad69d9749ae2e386170e5f05f471b6a313f5b10
César Omar Fernández Mendiola <omar.f.cesarin28@gmail.com>
2025-06-06 11:50:14 -0600

    notiasincronas final

--- app/Http/Controllers/MultasController.php
+++ app/Http/Controllers/MultasController.php
@@ -6,13 +6,13 @@ use App\Models\Multa;
 use Illuminate\Http\Request;
 
 class MultasController extends Controller
-{// Laravel - MultasController.php
+{
 public function index(Request $request)
 {
     if ($request->has('departamento_id')) {
         $multas = Multa::where('departamento_id', $request->get('departamento_id'))->get();
     } else {
-        $multas = Multa::all(); // 👉 Aquí devuelve todas si no hay filtro
+        $multas = Multa::all(); 
     }
 
     return response()->json($multas);
