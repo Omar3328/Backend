860b6ce7a7cde4bee2d6622f22e3129aef548c68
César Omar Fernández Mendiola <omar.f.cesarin28@gmail.com>
2025-06-13 11:33:13 -0600

    Backen-modal

--- app/Http/Controllers/MultasController.php
+++ app/Http/Controllers/MultasController.php
@@ -7,16 +7,31 @@ use Illuminate\Http\Request;
 
 class MultasController extends Controller
 {
-public function index(Request $request)
-{
-    if ($request->has('departamento_id')) {
-        $multas = Multa::where('departamento_id', $request->get('departamento_id'))->get();
-    } else {
-        $multas = Multa::all(); 
+    public function index(Request $request)
+    {
+        if ($request->has('departamento_id')) {
+            $multas = Multa::where('departamento_id', $request->get('departamento_id'))->get();
+        } else {
+            $multas = Multa::all();
+        }
+
+        return response()->json($multas);
     }
 
-    return response()->json($multas);
-}
+    public function store(Request $request)
+    {
+        $request->validate([
+            'descripcion' => 'required|string',
+            'departamento_id' => 'nullable|exists:departamentos,id',
+            'valor' => 'required|numeric|min:0'
+        ]);
 
+        $multa = new Multa();
+        $multa->descripcion = $request->descripcion;
+        $multa->departamento_id = $request->departamento_id;
+        $multa->valor = $request->valor;
+        $multa->save();
 
+        return response()->json($multa, 201);
+    }
 }
